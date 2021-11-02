f5febe288df2514321ebaaac26df5f7e8eefa98c
Evelyn9521 <evelyn9521@outlook.es>
2021-11-02 14:33:04 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html' //Nos muestra lo que hay en ese archivo
+  //El template <span>Hola</span> muestra solo lo que hay aquí dentro
 })
+
+
 export class AppComponent {
-  title = 'bases';
+ 
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+/* La Aplicacion en general, lo que contiene nuestra aplicacion*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//modulos
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //se colocan los componentes individuales que no tienen modulos
+    AppComponent,
+
+    
+   
   ],
-  imports: [
-    BrowserModule
+  imports: [ //se colocan modulos agrupados entre sí
+    BrowserModule,
+    HeroesModule,  //modulo de heroes
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+//para crear un proyecto:
+// ng new nombre del proyecto
+//para levantar servidor ng serve -o
+//Para crear un componente automaticamente se utiliza el comando ng generate component nombre de la carpeta/nombre del componente
+//crear un componente version simplificada ng g c nombre carpeta/ nombre componente
